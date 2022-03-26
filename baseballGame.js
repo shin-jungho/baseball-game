a404024fd300818c7aa41acc65399e682e90d95f
jungho shin <wjdghtls11@gmail.com>
2022-03-26 19:10:46 +0900

    feat: compare input numbers with answer

--- baseballGame.js
+++ baseballGame.js
@@ -29,3 +29,41 @@ function checkInput(input) {
   return true; 
 }
 
+// 입력값의 대한 코드
+let nothing = 0;
+formTag.addEventListener('submit', (e) => {
+  e.preventDefault(); // 기본 동작 막기위해서 사용
+  const value = inputTag.value;
+  inputTag.value = '';
+  const valid = checkInput(value);
+  if (!valid) { // 입력값 문제 없을때
+    return;
+  } 
+
+  // 정답일때
+  if(answer.join('') === value) { // join 배열 -> 문자열로 바꿈 , split 문자열 -> 배열
+    logsTag.textContent = '정답입니다!';
+    restartGame();
+    return;
+  }
+  
+  // 스트라이크, 볼 갯수 검사하는 코드
+  let strike = 0;
+  let ball = 0;
+  for(let i = 0; i < answer.length; i++) {
+    const index = value.indexOf(answer[i]);
+    if(index >  -1) { // 일치하는 숫자 발견
+      if (index === i) { // 자릿수도 같음
+        strike += 1;
+      } else { // 숫자만 같음
+        ball += 1;
+      }
+    } 
+  }
+    if (strike === 0 && ball === 0){ // 스트라이크, 볼 둘 다 아닐때 낫싱
+      nothing++;
+      logsTag.append(`${value} >> 낫싱`, document.createElement('br'));
+    } else {
+      logsTag.append(`${value} >> 스트라이크: ${strike} 볼: ${ball} `, document.createElement('br'));
+    }
+});
